57481a05b21b2f5d824c8871df163c50556bc724
mas-dse-aegg <aegg@eng.ucsd.edu>
2015-11-13 17:58:54 -0800

    WIP

--- day_4_OOP_and_matplotlib/matplotlib/rain.py
+++ day_4_OOP_and_matplotlib/matplotlib/rain.py
@@ -8,16 +8,41 @@ Author: Nicolas P. Rougier
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from mpl_toolkits.basemap import Basemap
+import pandas as pd
 
+feed="http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/"
+feeds={1: feed + "significant_month.csv",
+      2: feed+ "4.5_month.csv",
+      3: feed+ "2.5_month.csv",
+      4: feed + "1.0_month.csv"}
+
+def download_data(i):
+    return pd.read_csv(feeds[i])
+
+stations=download_data(2)
+lons=stations['longitude'].values
+lats=stations['latitude'].values
+
+#General plot  initializations
+fig = plt.figure(figsize=(14,10))
+ax = plt.subplot(1,1,1)
+earth = Basemap(projection='mill')
+earth.drawcoastlines(color='0.50', linewidth=0.25)
+earth.fillcontinents(color='0.95')
+
+
+x, y = earth(lons,lats)
+earth.plot(x,y,'g.')
 
 # Create new Figure and an Axes which fills it.
-fig = plt.figure(figsize=(7, 7))
-ax = fig.add_axes([0, 0, 1, 1], frameon=False)
-ax.set_xlim(0, 1), ax.set_xticks([])
-ax.set_ylim(0, 1), ax.set_yticks([])
+
+# ax = fig.add_axes([0, 0, 1, 1], frameon=False)
+# ax.set_xlim(0, 1), ax.set_xticks([])
+# ax.set_ylim(0, 1), ax.set_yticks([])
 
 # Create rain data
-n_drops = 50
+n_drops = len(x)
 rain_drops = np.zeros(n_drops, dtype=[('position', float, 2),
                                       ('size',     float, 1),
                                       ('growth',   float, 1),
@@ -25,14 +50,19 @@ rain_drops = np.zeros(n_drops, dtype=[('position', float, 2),
 
 # Initialize the raindrops in random positions and with
 # random growth rates.
-rain_drops['position'] = np.random.uniform(0, 1, (n_drops, 2))
-rain_drops['growth'] = np.random.uniform(50, 200, n_drops)
+#chose 50 from our dataset 
+pairs = np.column_stack((x,y))
+# pairs = pairs[:n_drops]
+
+
+rain_drops['position'] = pairs #np.random.uniform(0, 1, (n_drops, 2)) *1000000
+rain_drops['growth'] = np.random.uniform(50, 200, n_drops) 
 
 # Construct the scatter which we will update during animation
 # as the raindrops develop.
 scat = ax.scatter(rain_drops['position'][:, 0], rain_drops['position'][:, 1],
                   s=rain_drops['size'], lw=0.5, edgecolors=rain_drops['color'],
-                  facecolors='none')
+                  facecolors='none', zorder=100)
 
 
 def update(frame_number):
@@ -48,7 +78,7 @@ def update(frame_number):
 
     # Pick a new position for oldest rain drop, resetting its size,
     # color and growth factor.
-    rain_drops['position'][current_index] = np.random.uniform(0, 1, 2)
+    # rain_drops['position'][current_index] = np.random.uniform(0, 1, 2)
     rain_drops['size'][current_index] = 5
     rain_drops['color'][current_index] = (0, 0, 0, 1)
     rain_drops['growth'][current_index] = np.random.uniform(50, 200)
